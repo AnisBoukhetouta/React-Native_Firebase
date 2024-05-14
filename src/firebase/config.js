c5879337e5fec08ec21b86b4948a766ad9a89cc5
AlexHong <AlexHong1913@gmail.com>
2024-05-14 10:45:52 +0900

    Build firebase structure

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+import {API_KEY,
+AUTH_DOMAIN,
+DATABASE_URL,
+PROJECT_ID,
+STORAGE_BUCKET,
+MESSAGING_SENDER_ID,
+APP_ID} from '@env';
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
